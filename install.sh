28f91ff260f2dd431c48d2733e48192fad829ffc
xkdzt <60614617+xkdzt@users.noreply.github.com>
2020-08-02 06:59:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -615,7 +615,7 @@ nginx_process_disabled() {
 #    judge "rc.local 配置"
 #}
 acme_cron_update() {
-    wget -N -P /usr/bin --no-check-certificate "https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/dev/ssl_update.sh"
+    wget -N -P /usr/bin --no-check-certificate "https://raw.githubusercontent.com/xkdzt/V2Ray_ws-tls_bash_onekey/master/ssl_update.sh"
     if [[ $(crontab -l | grep -c "ssl_update.sh") -lt 1 ]]; then
       if [[ "${ID}" == "centos" ]]; then
           #        sed -i "/acme.sh/c 0 3 * * 0 \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
